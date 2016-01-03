e236dc478e751af4cf4b07deb3c13c466aa351f2
huang-sh <huang_sh@163.com>
2016-01-03 20:28:07 +0800

    php配置项

--- build/php-nginx.conf.sh
+++ build/php-nginx.conf.sh
@@ -18,7 +18,12 @@ setDebug
 dir=`pwd`
 phpPath=$(cd "$dir/../php"; pwd)
 cd $dir/../source/php
-./configure --prefix=$phpPath --with-config-file-path=$phpPath/etc -with-curl --with-mysql --with-zlib --with-gd --with-mcrypt --with-jpeg-dir --with-png-dir --with-xpm-dir --with-gettext --with-freetype-dir --with-bz2 --with-iconv-dir --with-libxml-dir --enable-fpm --with-fpm-user=www --with-fpm-group=www --disable-pdo --enable-mbstring --enable-sockets --enable-soap --enable-gd-native-ttf --enable-gd-jis-conv --enable-ftp --enable-zip --enable-calendar --enable-mbstring --enable-exif --enable-fd-setsize=4096 --disable-ipv6 --enable-maintainer-zts $debug
+
+# 编译所有的扩展
+./configure --prefix=$phpPath --sbindir=$phpPath/bin/  --with-config-file-path=$phpPath/etc/ --with-config-file-scan-dir=$phpPath/etc/ --libexecdir=$phpPath/ext/ --with-curl --with-mysql --with-zlib --with-gd --with-mcrypt --with-jpeg-dir --with-png-dir --with-xpm-dir --with-gettext --with-freetype-dir --with-bz2 --with-iconv-dir --with-libxml-dir --enable-fpm --with-fpm-user=www --with-fpm-group=www --disable-pdo --enable-mbstring --enable-sockets --enable-soap --enable-gd-native-ttf --enable-gd-jis-conv --enable-ftp --enable-zip --enable-calendar --enable-mbstring --enable-exif --enable-fd-setsize=4096 --disable-ipv6 --enable-maintainer-zts $debug
+
+# 自定义扩展
+#./configure --prefix=$phpPath --with-config-file-path=$phpPath/etc --with-curl --with-mysql --with-zlib --with-gd --with-mcrypt --with-jpeg-dir --with-png-dir --with-xpm-dir --with-gettext --with-freetype-dir --with-bz2 --with-iconv-dir --with-libxml-dir --enable-fpm --with-fpm-user=www --with-fpm-group=www --disable-pdo --enable-mbstring --enable-sockets --enable-soap --enable-gd-native-ttf --enable-gd-jis-conv --enable-ftp --enable-zip --enable-calendar --enable-mbstring --enable-exif --enable-fd-setsize=4096 --disable-ipv6 --enable-maintainer-zts $debug
 
 sudo make clean && sudo make && sudo make install
 echo $s
