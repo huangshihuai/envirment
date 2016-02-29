9a42996e3ffc7f8c1486aecd1d241689e90089e4
huang-sh <huang_sh@163.com>
2016-02-29 20:20:32 +0800

    紫丁花

--- build/copyLIB.sh
+++ build/copyLIB.sh
@@ -7,29 +7,27 @@ function createDependDir() {
     mkdir -p "../lib/gcc-4.9.0"
     dependLib="$(cd '../lib/gcc-4.9.0'; pwd)"
 }
-dependLib="/root/envirment/lib"
 function copyLib() {
     PHP_PATH=$(cd "../php"; pwd)
     PHP_BIN_LIB="$(find $PHP_PATH -type f -exec file -i '{}' \; | egrep 'x-executable; charset=binary|x-sharedlib; charset=binary' | awk -F ': ' '{print $1}')"
     PHP_BIN="$(find $PHP_PATH -type f -exec file -i '{}' \; | grep 'x-executable; charset=binary' | awk -F ': ' '{print $1}')"
-    for i in "$PHP_BIN_LIB"; do
+    for i in $PHP_BIN_LIB; do
         deps="$(ldd $i | awk -F ' ' '{print $3}' | grep '.so')"
-        for j in "$deps"; do
-            if [ "$j" == "" ];then
+        for j in $deps; do
+            if [ "$j" = "" ]; then
                 continue
             fi
-            isLink="$(ls -l $j | awk -F ' -> ' '{print $2}')"
-            if [ ! "$isLink"="" ]; then
+            isLink="$(readlink $j)"
+            if [ "$isLink"="" ]; then
                 cp "$j" "$dependLib"
+                echo $j
                 continue
             fi
 
-            fileDir=$(dirname $j)
-            echo $fileDir
-            #echo "$isLink"
-            #cp -n "$fileDir/$isLink" "$dependLib"
+            echo $isLink
+            cp -n "$isLink" "$dependLib"
         done
     done
 }
-#createDependDir
+createDependDir
 copyLib
