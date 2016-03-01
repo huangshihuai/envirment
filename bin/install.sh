c75756e13f6096463dfcedcfef836e86a437e9ba
huang-sh <huang_sh@163.com>
2016-03-01 19:21:42 +0800

    增加替换文件路径

--- bin/install.sh
+++ bin/install.sh
@@ -2,16 +2,27 @@
 
 ROOT="$(cd ..; pwd)"
 LIBPATGH="$ROOT/lib/gcc-4.9.0"
+PHP="$ROOT/php/"
 patchelf="$ROOT/bin/patchelf"
 new_linker="$LIBPATGH/ld-linux-x86-64.so.2"
 if [ ! -f "$new_linker" ]; then
     echo "not found $new_linker"
     exit
 fi
+oldRoot=`cat ./config/php_config | grep ROOT | cut -d '=' -f 2`
+echo $oldRoot
+exit
 for line in `cat ./config/php_config`; do
+    if [ ! -f "$line" ]; then
+        echo "not fount: $line"
+        continue
+    fi
     old_linker=`$patchelf --print-interpreter $line`
     if [[ "$old_linker" == "$new_linker" ]];then
         continue
     fi
+    echo $patchelf
+    echo $new_linker
+    echo $line
     $patchelf --set-interpreter $new_linker $line
 done
