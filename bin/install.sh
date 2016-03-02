434dccc2957c544702ba1429c7e0895d24e030ad
huang-sh <huang_sh@163.com>
2016-03-02 19:41:54 +0800

    自定义

--- bin/install.sh
+++ bin/install.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+PHP_VERSION=5.6.17
+PHP_CONF=${}
+
 ROOT="$(cd ..; pwd)"
-LIBPATGH="$ROOT/lib/gcc-4.9.0"
+LIBPATH="$ROOT/lib/gcc-4.9.0"
 PHP="$ROOT/php/"
-patchelf="$ROOT/bin/patchelf"
-new_linker="$LIBPATGH/ld-linux-x86-64.so.2"
+patchelf="$ROOT/bin/patchelf" 
+new_linker="$LIBPATH/ld-linux-x86-64.so.2"
 if [ ! -f "$new_linker" ]; then
-    echo "not found $new_linker"
+    echo "<not found $new_linker>"
     exit
 fi
 oldRoot=`cat ./config/php_config | grep ROOT | cut -d '=' -f 2`
-echo $oldRoot
-exit
+if [[ "$oldRoot" != "$ROOT" ]]; then
+    echo "替换数据-路径替换"
+    sed -i "s:${oldRoot}:${ROOT}:g" `grep "${oldRoot}" -rl ./config/php_config`
+fi
 for line in `cat ./config/php_config`; do
     if [ ! -f "$line" ]; then
-        echo "not fount: $line"
         continue
     fi
     old_linker=`$patchelf --print-interpreter $line`
     if [[ "$old_linker" == "$new_linker" ]];then
         continue
     fi
-    echo $patchelf
-    echo $new_linker
-    echo $line
-    $patchelf --set-interpreter $new_linker $line
+    $patchelf --set-rpath "$LIBPATH" --force-rpath "$line"
+    $patchelf --set-interpreter "$new_linker" "$line"
 done
--- build/depend.sh
+++ build/depend.sh
@@ -9,6 +9,7 @@ function createDependDir() {
     dependLib="$(cd '../lib/gcc-4.9.0'; pwd)"
 }
 function copyLib() {
+    cp /lib64/ld-linux-x86-64.so.2 "$dependLib"
     PHP_PATH=$(cd "../php"; pwd)
     PHP_BIN_LIB="$(find $PHP_PATH -type f -exec file -i '{}' \; | egrep 'x-executable; charset=binary|x-sharedlib; charset=binary' | awk -F ': ' '{print $1}')"
     PHP_BIN="$(find $PHP_PATH -type f -exec file -i '{}' \; | grep 'x-executable; charset=binary' | awk -F ': ' '{print $1}')"
@@ -18,17 +19,10 @@ function copyLib() {
             if [ "$j" = "" ]; then
                 continue
             fi
-            isLink="$(readlink $j)"
-            if [ "$isLink"="" ]; then
-                cp -n "$j" "$dependLib"
-                continue
-            fi
-
-            cp -n "$isLink" "$dependLib"
+            cp -n "$j" "$dependLib"
         done
     done
-
-    2> "$ROOT/bin/config/php_config"
+    echo "ROOT=$ROOT"> "$ROOT/bin/config/php_config"
     for i in $PHP_BIN; do
         echo "$i" >> "$ROOT/bin/config/php_config"
 #        /root/envirment/bin/patchelf --set-rpath /root/envirment/lib/gcc-4.9.0 --force-rpath "$i"
