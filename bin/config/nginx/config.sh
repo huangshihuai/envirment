e06d5f3798442240b22dde2be7e352af2c5d4f14
huang-sh <huang_sh@163.com>
2016-04-10 22:44:13 +0800

    还未完成的nginx

---
+++ bin/config/nginx/config.sh
@@ -0,0 +1,7 @@
+old_dir='/root/envirment'
+binary_files=(
+    webserver/nginx/sbin/nginx
+)
+text_files=(
+    webserver/nginx/conf
+)
--- system_build/move-lib.sh
+++ system_build/move-lib.sh
@@ -4,9 +4,15 @@ ENV_ROOT=$(readlink -f `dirname $BASE_SOURCE[0]`/..)
 ENV_LIB_PATH=$ENV_ROOT/lib/gcc-4.9.0
 patchelf=$ENV_ROOT/bin/patchelf
 
+ii=0
 move_lib() {
     params=$1
     for param in ${params[*]}; do
+        if [ $ii -eq 1 ]; then
+            echo $param
+            continue
+        fi
+        ii=1
         fileName=$ENV_ROOT/$param
         if [[ ! -f $fileName ]]; then
             continue;
@@ -14,13 +20,17 @@ move_lib() {
         now_set_path='$ORIGIN:$ORIGIN/../lib:$ORIGIN/../../lib/gcc-4.9.0'
         old_set_path=`$patchelf --print-rpath "$fileName"`
         if [[ $now_set_path == $old_set_path ]]; then
-            continue;
+            echo 1
+            continue
         fi
         ld=`$patchelf --print-interpreter $fileName`
         if [[ ! -f $ld ]]; then
             continue;
         fi
-        cp $ld $ENV_LIB_PATH
+        ldDir=$(readlink -f `dirname $ld`)
+        if [[ $ldDir != $ENV_LIB_PATH ]]; then
+            cp $ld $ENV_LIB_PATH
+        fi
 
         deps=`ldd $fileName | awk -F ' ' '{print $3}' | grep '.so'`
         for dep in ${deps};do
